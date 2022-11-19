0593f91cae5c8612596b0718a015156f9d4810a3
@hexlet/cli <support@hexlet.io>
2022-11-19 11:16:21 +0300

    submit java-oop-ru/interfaces

--- java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
+++ java-oop-ru/interfaces/src/main/java/exercise/ReversedSequence.java
@@ -1,5 +1,35 @@
 package exercise;
 
 // BEGIN
+public class ReversedSequence implements CharSequence {
+    private final StringBuilder stringBuilder;
 
+    public ReversedSequence(String str) {
+        stringBuilder = new StringBuilder(str.length());
+
+        for (int i = 0; i < str.length(); i++) {
+            stringBuilder.append(str.charAt(str.length() - 1 - i));
+        }
+    }
+
+    @Override
+    public int length() {
+        return stringBuilder.length();
+    }
+
+    @Override
+    public char charAt(int index) {
+        return stringBuilder.charAt(index);
+    }
+
+    @Override
+    public CharSequence subSequence(int start, int end) {
+        return stringBuilder.subSequence(start, end);
+    }
+
+    @Override
+    public String toString() {
+        return stringBuilder.toString();
+    }
+}
 // END
--- java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
+++ java-oop-ru/interfaces/src/test/java/exercise/AppTest.java
@@ -57,6 +57,14 @@ class AppTest {
     }
 
     // BEGIN
-    
+    @Test
+    void testReversedSequence() {
+        CharSequence text = new ReversedSequence("abcdef");
+
+        assertThat(text.toString()).hasToString("fedcba");
+        assertThat(text.charAt(1)).isEqualTo('e');
+        assertThat(text.length()).isEqualTo(6);
+        assertThat(text.subSequence(1,4)).hasToString("edc");
+    }
     // END
 }
